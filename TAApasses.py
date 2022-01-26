47180ea09279763b960ba274ac3d77f6817a57d8
M. Atif Siddiqui <56283783+atifsiddiqui10@users.noreply.github.com>
2022-01-26 12:54:45 -0600

    Add files via upload

---
+++ TAApasses.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 26 12:36:49 2022
+
+@author: atifsiddiqui
+"""
+
+"""
+Created on Wed Jan 26 11:47:21 2022
+
+@author: atifsiddiqui
+@Reference FOT github 
+"""
+
+#Function to draw the pitch
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Size of the pitch in yards (!!!)
+pitchLengthX=120
+pitchWidthY=80
+
+#ID for England vs Sweden Womens World Cup
+match_id_required = 18245
+home_team_required ="Real Madrid"
+away_team_required ="Liverpool"
+
+file_name=str(match_id_required)+'.json'
+
+import json
+with open('/Users/atifsiddiqui/Documents/SoccerTact/open-data-master/data/events/'+file_name) as data_file:
+    #print (mypath+'events/'+file)
+    data = json.load(data_file)
+    
+#get the nested structure into a dataframe 
+#store the dataframe in a dictionary with the match id as key (remove '.json' from string)
+from pandas.io.json import json_normalize
+df = json_normalize(data, sep = "_").assign(match_id = file_name[:-5])
+
+#Find the passes
+passes = df.loc[df['type_name'] == 'Pass'].set_index('id')
+
+from FCPython import createPitch
+(fig,ax) = createPitch(pitchLengthX,pitchWidthY,'yards','gray')
+for i,thepass in passes.iterrows():
+    #if thepass['team_name']==away_team_required: #
+    if thepass['player_name']=='Trent Alexander-Arnold':
+        x=thepass['location'][0]
+        y=thepass['location'][1]
+        passCircle=plt.Circle((x,pitchWidthY-y),2,color="blue")      
+        passCircle.set_alpha(.2)   
+        ax.add_patch(passCircle)
+        dx=thepass['pass_end_location'][0]-x
+        dy=thepass['pass_end_location'][1]-y
+
+        passArrow=plt.Arrow(x,pitchWidthY-y,dx,-dy,width=3,color="blue")
+        ax.add_patch(passArrow)
+
+fig.set_size_inches(10, 7)
+#fig.savefig('Output/passes.pdf', dpi=100)
+plt.show()
