39a4b5330b27f1d5add49f55481718cb27bfb05f
M. Atif Siddiqui <56283783+atifsiddiqui10@users.noreply.github.com>
2022-02-02 19:27:50 -0600

    Team KPI Updated
    Added more KPI's

--- team_KPI.py
+++ team_KPI.py
@@ -22,7 +22,7 @@ away_team_required ="Liverpool"
 file_name=str(match_id_required)+'.json'
 
 import json
-with open('/Users/deepak/Documents/SoccerTact/data/events/'+file_name) as data_file:
+with open('/Users/atifsiddiqui/Documents/SoccerTact/open-data-master/data/events/'+file_name) as data_file:
     #print (mypath+'events/'+file)
     data = json.load(data_file)
     
@@ -31,25 +31,56 @@ with open('/Users/deepak/Documents/SoccerTact/data/events/'+file_name) as data_f
 from pandas.io.json import json_normalize
 df = json_normalize(data, sep = "_").assign(match_id = file_name[:-5])
 
-
 Liv_data =  df.loc[df['team_name'] == 'Liverpool'].set_index('id')
 real_data =  df.loc[df['team_name'] == 'Real Madrid'].set_index('id')
+
+new_df = real_data[real_data['location'].notna()]
 
 def RealteamKPI():
     
     total_shots = 0
     total_passes = 0
+    shots_from_penalty_area = 0; 
+    penalty_box_entry = 0; 
+    actions_PB = 0
+    total_corners = 0
+    total_freekicks = 0
     
+    
+    for i, events in new_df.iterrows():
+         x = events['location'][0]
+         y = events['location'][1]
+   
+         if(x > 102.0 and (y > 18.0 and y < 62.0)): 
+            penalty_box_entry = penalty_box_entry + 1
+            
+            
+            if (events['type_name'] == 'Shot'):
+                shots_from_penalty_area = shots_from_penalty_area + 1
+                
     
     for i, events in real_data.iterrows():
+        
         if (events['type_name'] == "Shot"):
             total_shots = total_shots + 1
         elif (events['type_name'] == "Pass"):
             total_passes = total_passes + 1
+            if (events['pass_type_name'] == "Corner"):
+                total_corners = total_corners + 1
+            if (events['pass_type_name'] == 'Free Kick'):
+                total_freekicks = total_freekicks + 1
+            
+                
+            
+           
     print("=================")
     print("Real Madrid Stats")
     print("Total Shots " + str(total_shots))
     print("Total Passes " + str(total_passes))
+    print("Penalty Box Entries " + str(penalty_box_entry))
+    print("Shots from penalty area " + str(shots_from_penalty_area))
+    print("Total Corners " + str(total_corners))
+    print("Total Free Kicks " + str(total_freekicks))
     print("=================")
 
 def LivteamKPI():
