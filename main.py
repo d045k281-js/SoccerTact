ef26646c9d4b00fb62fe0408156c0f74cba0aee4
Rahul <rahulpurswani99@gmail.com>
2022-02-08 16:52:51 -0600

    Uploaded the base main.py. Produces shots map for both the teams

---
+++ main.py
@@ -0,0 +1,27 @@
+#Function to draw the pitch
+from shots import generate_shots
+import sys
+import requests
+import json
+from pandas import json_normalize
+
+m_id = str(sys.argv[1])
+to_fetch = m_id+'.json'
+
+e_site = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/"+to_fetch
+e_data = json.loads((requests.get(e_site)).text)
+
+l_site = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/lineups/"+to_fetch
+l_data = json.loads((requests.get(l_site)).text)
+
+#getting the name of the teams
+df = json_normalize(l_data, sep = "_").assign(match_id = m_id)
+events = df.loc[df['match_id'] == m_id]
+t1 ='a'
+for i,event in events.iterrows():
+    if(t1=='a'):
+        t1 = event['team_name']
+    else:
+        t2 = event['team_name']
+
+generate_shots(m_id,t1,t2,e_data,l_data)
